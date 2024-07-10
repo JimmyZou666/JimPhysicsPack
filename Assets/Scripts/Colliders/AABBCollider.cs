4a286a6166997d032e8ca2af8b245c65e12ca5e1
jimmiezou <jimmiezou@tencent.com>
2024-07-10 14:09:51 +0800

    建立基本框架。

---
+++ Assets/Scripts/Colliders/AABBCollider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AABBCollider : MonoBehaviour, ICollider
+{
+    public Vector3 min = Vector3.one * -1;
+    public Vector3 max = Vector3.one;
+
+    private void OnDrawGizmos()
+    {
+        // 绘制AABB边界
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(transform.position, max - min);
+    }
+}
---
+++ Assets/Scripts/CollisionAlgorithms/AABBCollisionAlgorithm.cs
@@ -0,0 +1,13 @@
+﻿public class AABBCollisionAlgorithm : ICollisionAlgorithm
+{
+    public bool IsColliding(ICollider a, ICollider b)
+    {
+        AABBCollider box1 = a as AABBCollider;
+        AABBCollider box2 = b as AABBCollider;
+
+        // 检查每个轴上的投影是否重叠
+        return (box1.min.x <= box2.max.x && box1.max.x >= box2.min.x) &&
+               (box1.min.y <= box2.max.y && box1.max.y >= box2.min.y) &&
+               (box1.min.z <= box2.max.z && box1.max.z >= box2.min.z);
+    }
+}
