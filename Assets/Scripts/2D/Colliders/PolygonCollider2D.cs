715cbdc572595a6e25e8ad4fa5529150b61bdf72
jimmiezou <jimmiezou@tencent.com>
2024-07-14 21:34:45 +0800

    增加 | 增加了PolygonCollider和绘制方法。

---
+++ Assets/Scripts/2D/Colliders/PolygonCollider2D.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JimPhysicsPack
+{
+    public class PolygonCollider2D : Collider2D
+    {
+        public List<Vector2> points;
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.green;
+            if (points.Count > 1)
+            {
+                for (int i = 0; i < points.Count - 1; i++)
+                {
+                    Gizmos.DrawLine(points[i], points[i + 1]);
+                }
+                Gizmos.DrawLine(points[points.Count - 1], points[0]);
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Editor/PolygonCollider2DEditor.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+namespace JimPhysicsPack
+{
+    [CustomEditor(typeof(PolygonCollider2D))]
+    public class PolygonCollider2DEditor : Editor
+    {
+        private PolygonCollider2D polygonCollider;
+
+        private void OnEnable()
+        {
+            polygonCollider = (PolygonCollider2D)target;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            if (GUILayout.Button("Add Point"))
+            {
+                polygonCollider.points.Add(new Vector2());
+            }
+        }
+
+        private void OnSceneGUI()
+        {
+            Event guiEvent = Event.current;
+
+            if (guiEvent.type == EventType.MouseDown && guiEvent.button == 0 && guiEvent.control)
+            {
+                float closestDistance = float.MaxValue;
+                int closestIndex = -1;
+
+                for (int i = 0; i < polygonCollider.points.Count; i++)
+                {
+                    float distance = Vector2.Distance(polygonCollider.points[i], HandleUtility.GUIPointToWorldRay(guiEvent.mousePosition).origin);
+                    if (distance < closestDistance)
+                    {
+                        closestDistance = distance;
+                        closestIndex = i;
+                    }
+                }
+
+                if (closestIndex != -1)
+                {
+                    Undo.RecordObject(polygonCollider, "Remove Point");
+                    polygonCollider.points.RemoveAt(closestIndex);
+                    guiEvent.Use();
+                }
+            }
+
+            for (int i = 0; i < polygonCollider.points.Count; i++)
+            {
+                Handles.color = new Color(0, 0, 1, 1f);
+                EditorGUI.BeginChangeCheck();
+                Vector2 newTargetPosition = Handles.FreeMoveHandle(polygonCollider.points[i], Quaternion.identity, 0.03f, new Vector2(0.5f, 0.5f), Handles.DotHandleCap);
+                if (EditorGUI.EndChangeCheck())
+                {
+                    Undo.RecordObject(polygonCollider, "Move Point");
+                    polygonCollider.points[i] = newTargetPosition;
+                }
+            }
+        }
+    }
+}
