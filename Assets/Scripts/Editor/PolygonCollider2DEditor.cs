59d92feca94be0f5bfff61ab39c91eafcfd40ea3
jimmiezou <jimmiezou@tencent.com>
2024-07-14 23:04:26 +0800

    增加 | 闵可夫斯基差集可视化脚本

--- Assets/Scripts/Editor/PolygonCollider2DEditor.cs
+++ Assets/Scripts/Editor/PolygonCollider2DEditor.cs
@@ -12,6 +12,10 @@ namespace JimPhysicsPack
         private void OnEnable()
         {
             polygonCollider = (PolygonCollider2D)target;
+            if (polygonCollider.points == null)
+            {
+                polygonCollider.points = new List<Vector2>();
+            }
         }
 
         public override void OnInspectorGUI()
@@ -28,11 +32,13 @@ namespace JimPhysicsPack
         {
             Event guiEvent = Event.current;
 
+            // 如果按住ctrl并且点击了左键，会删除点击位置最近的点
             if (guiEvent.type == EventType.MouseDown && guiEvent.button == 0 && guiEvent.control)
             {
                 float closestDistance = float.MaxValue;
                 int closestIndex = -1;
 
+                // 寻找最近的点
                 for (int i = 0; i < polygonCollider.points.Count; i++)
                 {
                     float distance = Vector2.Distance(polygonCollider.points[i], HandleUtility.GUIPointToWorldRay(guiEvent.mousePosition).origin);
@@ -55,10 +61,13 @@ namespace JimPhysicsPack
             {
                 Handles.color = new Color(0, 0, 1, 1f);
                 EditorGUI.BeginChangeCheck();
+                // 绘制一个可自由移动的Handle
                 Vector2 newTargetPosition = Handles.FreeMoveHandle(polygonCollider.points[i], Quaternion.identity, 0.03f, new Vector2(0.5f, 0.5f), Handles.DotHandleCap);
                 if (EditorGUI.EndChangeCheck())
                 {
+                    // 记录信息，为了响应撤销
                     Undo.RecordObject(polygonCollider, "Move Point");
+                    // 赋值新位置
                     polygonCollider.points[i] = newTargetPosition;
                 }
             }
---
+++ Assets/Scripts/Misc/MinkowskiDifferenceVisualization.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace JimPhysicsPack
+{
+    [RequireComponent(typeof(PolygonCollider2D))]
+    [ExecuteInEditMode]
+    public class MinkowskiDifferenceVisualization : MonoBehaviour
+    {
+        public PolygonCollider2D polygon1, polygon2;
+        public PolygonCollider2D resultPolygon;
+
+        public void Awake()
+        {
+            resultPolygon = GetComponent<PolygonCollider2D>();
+
+            if (resultPolygon.points == null)
+                resultPolygon.points = new List<Vector2>();
+        }
+        private void Update()
+        {
+            if (!Application.isPlaying)
+            {
+                if (polygon1 != null && polygon2 != null && polygon1.points != null && polygon2.points != null)
+                    resultPolygon.points = MinkowskiDifference(polygon1.points, polygon2.points);
+            }
+        }
+
+        private List<Vector2> MinkowskiDifference(List<Vector2> polygon1, List<Vector2> polygon2)
+        {
+            List<Vector2> result = new List<Vector2>();
+
+            foreach (Vector2 point1 in polygon1)
+            {
+                foreach (Vector2 point2 in polygon2)
+                {
+                    result.Add(point1 - point2);
+                }
+            }
+
+            return result;
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.black;
+            Gizmos.DrawSphere(Vector3.zero, 0.02f);
+        }
+    }
+}
